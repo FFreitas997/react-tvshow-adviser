00b6ddc28fa8f734a78be97f734dc35a6b8f96fc
FFreitas <francisco.freitas.ff@gmail.com>
2023-11-21 11:31:39 +0000

    .env file

--- src/api/tv-show.js
+++ src/api/tv-show.js
@@ -4,17 +4,17 @@ import {baseURL, apiKeyTMDB} from "../configuration"
 export class TVShowAPI {
 
     static async fetchPopulars() {
-        const response = await axios.get(`${baseURL}tv/popular?api_key=${apiKeyTMDB}`);
+        const response = await axios.get(`${baseURL}tv/popular?api_key=${process.env.REACT_APP_API_KEY_PARAM}`);
         return response.data.results;
     }
 
     static async fetchRecommendations(tvShowID) {
-        const response = await axios.get(`${baseURL}tv/${tvShowID}/recommendations?api_key=${apiKeyTMDB}`)
+        const response = await axios.get(`${baseURL}tv/${tvShowID}/recommendations?api_key=${process.env.REACT_APP_API_KEY_PARAM}`)
         return response.data.results;
     }
 
     static async fetchSearch(input) {
-        const response = await axios.get(`${baseURL}search/tv?api_key=${apiKeyTMDB}&query=${input}`)
+        const response = await axios.get(`${baseURL}search/tv?api_key=${process.env.REACT_APP_API_KEY_PARAM}&query=${input}`)
         return response.data.results;
     }
 
--- src/configuration.js
+++ src/configuration.js
@@ -1,4 +1,3 @@
-export const apiKeyTMDB = 'a3c5d309a4c5bd8e605cbc301d68e072';
 export const baseURL = 'https://api.themoviedb.org/3/'
 export const backdropBaseURL = 'https://image.tmdb.org/t/p/original'
 export const smallImageCoverBaseURL = 'https://image.tmdb.org/t/p/w300';
